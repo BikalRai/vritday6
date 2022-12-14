534b8ceb28fcf608b43bec852c83b60e9c268386
Bikal Rai <bikalrai2785@gmail.com>
2022-12-14 17:44:25 +0545

    loop added with array looping

--- js/day6.js
+++ js/day6.js
@@ -1,31 +1,74 @@
-const person = {
-    person1: "Bikal",
-    person2: "Sally",
-};
+// const person = {
+//     person1: "Bikal",
+//     person2: "Sally",
+// };
 
-if (person.person1 !== "Bikal") {
-    console.log(`I am ${person.person1}`);
-    // console.log("I am " + person.person1);
-    // console.log("i am", person.person1);
-} else {
-    console.log(`I am ${person.person2}`);
-}
+// if (person.person1 !== "Bikal") {
+//     console.log(`I am ${person.person1}`);
+//     // console.log("I am " + person.person1);
+//     // console.log("i am", person.person1);
+// } else {
+//     console.log(`I am ${person.person2}`);
+// }
 
-const personTwo = {
-    name: "Timmy",
-    age: 19,
-};
+// const personTwo = {
+//     name: "Timmy",
+//     age: 19,
+// };
 
-if (personTwo.age > 20) {
-    console.log(`I am ${personTwo.name}`);
-}
+// if (personTwo.age > 20) {
+//     console.log(`I am ${personTwo.name}`);
+// }
 
-if (personTwo.name === "Timmy" && personTwo.age >= 20) {
-    console.log(`I am ${personTwo.name} and i am ${personTwo.age} years old!`);
-} else if (personTwo.name === "Timmy" && personTwo.age < 20) {
-    console.log("Failed to execute!");
-}
+// if (personTwo.name === "Timmy" && personTwo.age >= 20) {
+//     console.log(`I am ${personTwo.name} and i am ${personTwo.age} years old!`);
+// } else if (personTwo.name === "Timmy" && personTwo.age < 20) {
+//     console.log("Failed to execute!");
+// }
 
-if (personTwo.name === "Timmy" || personTwo.age > 20) {
-    console.log(`Yes i am ${personTwo.name} or ${personTwo.age} years old!`);
-}
+// if (personTwo.name === "Timmy" || personTwo.age > 20) {
+//     console.log(`Yes i am ${personTwo.name} or ${personTwo.age} years old!`);
+// }
+
+// // functions
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(10, 20));
+// console.log(add(30, 40));
+
+// const x = prompt("Enter number:");
+// const y = prompt("Enter number");
+// function mul(a, b) {
+//     return a - b;
+// }
+
+// console.log(mul(x, y));
+
+// function greaterNumber(a, b) {
+//     if (a > b) {
+//         return "Number one is greater than number two";
+//     }
+// }
+
+// greaterNumber(x, y);
+
+// function greater() {
+//     const a = 44;
+//     const b = 45;
+
+//     if (!(a > b)) {
+//         return `Number one is greater than number two!`;
+//     } else {
+//         return `Number two is greater than number one!`;
+//     }
+// }
+
+// console.log(greater());
+
+for (let i = 0; i < 5; i++) console.log("Value of i:", i);
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (let i = 0; i < arr.length; i++) console.log("Value: ", arr[i]);
